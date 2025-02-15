5f6be6f9f921d2ee7246323f3cc024ed1caf9083
Yogendiran-SP <jyogendiran@gmail.com>
2025-02-15 10:34:49 +0530

    income.csv cleaned

--- problem_1.py
+++ problem_1.py
@@ -27,18 +27,21 @@ data['SalStat'] = data['SalStat'].astype('category')
 #Checking the data types
 print(data.dtypes)
 
-# Checking for missing values
-print(data.isnull().sum())
-
 # Filling the missing values
+print("\nMissing Values of occupation:")
 data['occupation']=data['occupation'].fillna(data['occupation'].mode()[0])
 print(data['occupation'].isnull().sum())
+print("\nMissing Values of JobType:")
 data['JobType']=data['JobType'].fillna(data['JobType'].mode()[0])
 print(data['JobType'].isnull().sum())
+print("\nMissing Values of gender:")
+data['gender']=data['gender'].fillna(data['gender'].mode()[0])
+print(data['gender'].isnull().sum())
 
 # Printing Fill the missing values
 print("\nOcupation:",data['occupation'].value_counts())
 print("\nJobType:",data['JobType'].value_counts())
+print("\nGender:",data['gender'].value_counts())
 
 # Checking the datatype of occupation
 print("\nOccupation:")
@@ -49,5 +52,9 @@ print("\ngender:")
 print(data['gender'].unique())
 
 # Checking datatype of missing values
+print("\nMissing Rows of the whole data:")
 missing = data[data.isnull().any(axis = 1)]
 print(missing)
+
+# Checking for missing values
+print("Missing Values:",data.isnull().sum())
